7ce01268e0682d2a5f30e6885e971f6e09d65b5a
Wilfred Vásquez <wilfredvas@gmail.com>
2022-09-02 21:46:25 -0400

    Update nextjs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -54,7 +54,7 @@ export function Footer() {
             </Link>
           </div>
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
-            Copyright &copy; {new Date().getFullYear()} TaxPal. All rights
+            Copyright &copy; {new Date().getFullYear()} StartHere. All rights
             reserved.
           </p>
         </div>
